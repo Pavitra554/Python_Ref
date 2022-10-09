1ae5a2a458a335ac217f36cc87ab050a802fe829
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-10-09 18:30:10 +0530

    ✅ Strings in py

---
+++ 07_strings.py
@@ -0,0 +1,25 @@
+# single line string
+str = "Hello"
+
+# Multiline string
+str1 = '''
+hello 
+world
+'''
+
+print(str)
+print(str1)
+
+name = "pavitra"
+
+print(name[-2])  # r
+print(name[2])  # v
+print(name[2:])  # vitra
+print(name[:5])  # pavit
+print(name[2:4])  # vi
+
+
+# slicing string
+print(name[::-1])
+
+print(name[-1:0:-1])
