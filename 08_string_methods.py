464a02b6f4eb357099ab9eb6c1bb28bfdda33c04
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-10-09 18:30:22 +0530

    ✅ String methods in py

---
+++ 08_string_methods.py
@@ -0,0 +1,12 @@
+str = "python"
+
+# trying string methods
+
+print("isalpha() --> ", str.isalpha())
+print("isdigit() --> ", str.isdigit())
+print("islower() --> ", str.islower())
+print("isupper() --> ", str. isupper())
+print("upper() --> ", str. upper())
+print("lower() --> ", str. lower())
+
+# print(str.rstrip())
