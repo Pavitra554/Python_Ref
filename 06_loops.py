837914a96e21d87c5ab4249f3a28e6106cc3f8f4
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-10-09 18:29:50 +0530

    ✅ Loops in py

---
+++ 06_loops.py
@@ -0,0 +1,27 @@
+# For Loop
+for x in range(10):
+    print(x, end=" ")
+
+print()
+
+for x in range(10):
+    print(x*2, end=", ")
+
+print()
+str = "Pavitra"
+
+for c in str:
+    print(c, end=" ")
+
+print()
+l = ["hello", "how", "are", "you"]
+
+for e in l:
+    print(e, end=" ")
+
+print()
+# while loops
+n = 10
+while n != 0:
+    print(n, end=" ")
+    n -= 1
