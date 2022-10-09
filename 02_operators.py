cf2ecb97f189afde9fc919ca90c1008b8ffd5132
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-10-09 18:28:25 +0530

    ✅ Operators in py

---
+++ 02_operators.py
@@ -0,0 +1,23 @@
+# Arithematic Operators
+# +, -, *, /, and % are same as other.
+a = 7
+b = 3
+print(a//b)  # gives only quotient
+
+print(a**b)  # a^b
+
+
+# Assignment Operators
+# +=, -=, *= /= and %= are same as other languages (java,dart,ts);
+
+# ** for power
+# and // for quotient
+
+
+# Relational Operators
+
+# <, >, <=, >=, !=, ==
+
+# Logical Operators
+
+# and or not  (same as other).
