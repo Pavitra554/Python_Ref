847af5ee948f237ad1eeea0596ea0e5674b382a2
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-10-09 18:30:50 +0530

    ✅ List methods in py

---
+++ 10_list_methods.py
@@ -0,0 +1,35 @@
+# List methods
+
+a = [8, 4, 2]
+print(a)
+
+a.append(0)
+print(a)
+
+a.insert(2, 2)
+print(a)
+
+a.sort()
+print(a)
+
+a.pop(2)
+print(a)
+
+a.reverse()
+print(a)
+
+print(a.index(2))
+
+print(a.count(2))
+
+# a.clear()
+# print(a)
+
+# List functions
+print(len(a))
+print(max(a))
+print(min(a))
+print(sum(a))
+
+str = "pavitra"
+print(list(str))
