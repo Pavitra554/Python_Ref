dbe2483cc6a68e343ea7f04ab9d10de70855c9a9
PAVITRA BEHARA <pavitraredmi@gmail.com>
2022-10-09 18:28:08 +0530

    ✅ Basic syntax

---
+++ 01_basic.py
@@ -0,0 +1,21 @@
+print("hello world")
+
+# Data types
+name = "pavitra"
+age = 20
+isOkay = False
+rating = 4.2
+
+# complex number
+c = 4 + 5j
+
+# list
+list = ["hello", "world"]
+
+# printing types
+print('name --> ', type(name))
+print('age --> ', type(age))
+print('isOkay --> ', type(isOkay))
+print('rating --> ', type(rating))
+print('c --> ', type(c))
+print('list --> ', type(list))
